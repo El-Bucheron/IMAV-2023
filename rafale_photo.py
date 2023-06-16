43bb79b62b39d5122620ee87dd766e78175dea3a
El-Bucheron <mat.manducher@gmail.com>
2023-06-16 15:23:35 +0200

    Amélioration du code de prise de photos
    Modification de la temporisation entre les photos de 2 à 0.5 seconde
    Ajout de commentaire pour expliquer le code
    Modification de la nomenclature des photos au format "heures heure:minute:seconde latitude,longitude,altitude.jpg  pour avoir les photos triès par ordre chronologique dans l'explorateur de fichiers
    Le programme ne prend des photos que si le drone est à plus d'un mètre du sol pour éviter d'aboir trop de photos inutilisables

--- rafale_photo.py
+++ rafale_photo.py
@@ -1,4 +1,19 @@
-# import the necessary package
+"""
+Code prenant des photos en continue avec la PiCamera
+
+Paramètres attendus :
+    (optionnel) Nom du dossier qui stockera les photos enregistrées
+    
+Spécificités:
+    L'intervalle entre les photos est de 0.5 secondes
+    La nomenclatrue des photos est la suivante : "heure:minute:seconde latitude,longitude,altitude.jpg 
+    Le code s'arrête avec Ctrl+C
+    Quand le drone est à moins d'un mètre du sol, aucune photo n'est prise
+    Les photos prises durant un même appel de programme seront stockées dans un dossier qui sera crée en début de code
+    Les précedents dossiers sont eux-mêmes contenus dans un dossier "photos" qui sera créé s'il n'existe pas au moment de l'appel du code
+"""
+
+# Imports
 from time import sleep
 from datetime import datetime
 from commande_drone import Drone
@@ -6,26 +21,47 @@ from detection_target import Detection
 import os
 import sys
 
-path = "/home/housso97/Desktop/2023/random IMAV 2023/Test vol drone 14 juin/images_prises/"
+# Chemins absolu du dossier contenant les dossiers de photos
+path = os.path.dirname(os.getcwd()) + "/photos/"
+# Si ce dossier n'existe pas, on le créé
+try:
+    os.mkdir(path)
+# Si le dossier existe déjà, on ne fait rien    
+except FileExistsError:
+    pass
 
+
+#On recupère le nom de dossier fourni par l'utilisateur s'il en a fourni un
 try:
     nom_dossier = sys.argv[1]
+#Si l'utilisateur n'a pas fourni de nom de dossier, on utilse la date et l'heure d'appel du code pour le nommer    
 except IndexError:
-    nom_dossier = datetime.now().strftime("%d-%m %H:%M:%S")
+    nom_dossier = datetime.now().strftime("%d-%m %H:%M:%S") + "/"
+#Création du dossier
 os.mkdir(path + nom_dossier)
     
+    
+# Instanciation d'un objet de type drone pour obtenir ses coordonnées GPS et d'un objet de type Detection pour prendre les photos   
 drone = Drone()
 camera = Detection()
 
+
 print("Début de programme")
 try:
+    # Boucle infinue pour capture les photos en continu
     while True:
-        camera.prise_photo(path + nom_dossier + "/"+                                     # Chemin du dossier
-                           str(drone.vehicle.location.global_relative_frame.lat) + "," + # Encodage de la Latitude
-                           str(drone.vehicle.location.global_relative_frame.lon) + "," + # Encodage de la longitude
-                           str('%.2f'%(drone.vehicle.rangefinder.distance)) + " " +      # Encodage de l'altitude
-                           datetime.now().strftime("%H:%M:%S") + ".jpg")                 # Heure de prise de la photo
-        sleep(1)
-        print("Photo prise")
+        # Si le drone est à plus d'un mètre du sol la photo est prise
+        if(drone.vehicle.rangefinder.distance > 1):
+            # Prise de la photo et stockage selon la nomenclatrue décrite en début de code 
+            camera.prise_photo(path + nom_dossier +                                          # Chemin du dossier
+                               datetime.now().strftime("%H:%M:%S") + " " +                   # Heure de prise de la photo  
+                               str(drone.vehicle.location.global_relative_frame.lat) + "," + # Encodage de la Latitude
+                               str(drone.vehicle.location.global_relative_frame.lon) + "," + # Encodage de la longitude
+                               str('%.2f'%(drone.vehicle.rangefinder.distance)) + ".jpg ")   # Encodage de l'altitude
+            # Temporisation
+            sleep(0.5)
+            print("Photo prise")
+
+# Arret du programme avec le Ctrl+C
 except KeyboardInterrupt:
     print("Fin de programme")
