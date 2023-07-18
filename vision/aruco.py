804313715dccca09ab6808acbc3c73fd64995da7
El-Bucheron <mat.manducher@gmail.com>
2023-07-18 16:35:06 +0200

    Correction code affichage asservissement sur les photos

--- vision/aruco.py
+++ vision/aruco.py
@@ -13,6 +13,9 @@ from datetime import datetime
 from commande_drone import Drone
 from utilities import creation_dossier_photo, enregistrement_photo_date_position
 
+
+drone = Drone()
+
 # On recupère le nom de dossier fourni par l'utilisateur s'il en a fourni un
 # Sinon on utilse la date et l'heure d'appel du code pour le nommer  
 try:
@@ -22,9 +25,6 @@ except IndexError:
 # Création du dossier de photos
 chemin_dossier = creation_dossier_photo(nom_dossier)
 
-
-drone = Drone()
-
 print("Début de programme")
 # Structure "try" pour pouvoir arrêter le programme avec un Ctrl+C
 try:
@@ -33,8 +33,8 @@ try:
         centre_aruco_X, centre_aruco_Y, aruco_id, image = drone.camera.detection_aruco(True)
         print(("Aruco détecté" if centre_aruco_X != None else "Aruco non détecté")+ " with id " + str(aruco_id) + " altitude: " + str('%.2f'%(drone.vehicle.rangefinder.distance)))
         erreurX, erreurY, vx, vy = drone.asservissement_atterrissage_2022(centre_aruco_X, centre_aruco_Y)
-        message_image = "Erreur : EX = " + str(erreurX) + " ; EY = " + str(erreurY) + "\nVitesse : Vx = " + str(vx) + " ; Vy = " + str(vy)
-        cv2.putText(image, message_image, (0, 0), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+        image = cv2.putText(image, "Erreur : EX = " + str(erreurX) + " ; EY = " + str(erreurY), (0, 25), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+        image = cv2.putText(image, "Vitesse : Vx = " + str(vx) + " ; Vy = " + str(vy), (0, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
         # Traçage d'un cercle au centre de l'image
         cv2.circle(image, (drone.camera.x_imageCenter, drone.camera.y_imageCenter), 4, (0, 255, 0), -1)
         # Sauvegarde de la photo
